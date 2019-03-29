51b0f780c1c38c0b9054310590dc3c447a08aacb
Don Le Quy <don.lequy@elinext.com>
2019-03-29 23:57:48 +0700

    donlq - updates - skeleton - add sample component

---
+++ src/common/_Sample/Sample.vue
@@ -0,0 +1,28 @@
+<template lang="pug" src="./Sample.pug"></template>
+
+<script>
+
+export default {
+  name: 'Sample',
+  props: {
+    data: {
+      type: Object,
+      default: () => null,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss">
+.sample {
+
+}
+</style>
+
