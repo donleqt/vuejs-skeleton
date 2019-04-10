53fbbb38426792225da9490a065a7b63472f8d74
Don Le Quy <don.lequy@elinext.com>
2019-04-10 23:26:22 +0700

    donlq - updates - SKELETON - Add helper reload, fix docs

---
+++ src/helpers/vue/reload.js
@@ -0,0 +1,22 @@
+export default function withReload(component) {
+  return {
+    data() {
+      return {
+        toggle: false,
+      };
+    },
+    render(h) {
+      return <component key={this.toggle} reload={this.reload}/>;
+    },
+    watch: {
+      $route() {
+        this.reload();
+      },
+    },
+    methods: {
+      reload() {
+        this.toggle = !this.toggle;
+      },
+    },
+  };
+}
