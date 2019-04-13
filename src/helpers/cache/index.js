811d451097d9c10c9c3f79f6c430c213b5c27e37
Don Le Quy <don.lequy@elinext.com>
2019-04-13 10:09:41 +0700

    donlq - updates - SKELETON optimize helper lazy cache

--- src/helpers/cache/index.js
+++ src/helpers/cache/index.js
@@ -1,16 +1,27 @@
 function createCacheInstance() {
   const storage = {};
   const pending = {};
+
+  const runAsyncFunction = (name, asyncFunction) => {
+    return asyncFunction().then((res) => {
+      storage[name] = res;
+      return res;
+    }).catch(err => delete storage[name]);
+  };
+
   return {
     async get(name, asyncFunction) {
       if (storage[name]) {
-        pending[name] = asyncFunction().then(res => storage[name] = res);
+        pending[name] = runAsyncFunction(name, asyncFunction);
         return storage[name];
       }
       if (!pending[name]) {
-        pending[name] = asyncFunction().then(res => storage[name] = res);
+        pending[name] = runAsyncFunction(name, asyncFunction);
       }
       return pending[name];
+    },
+    delete(name) {
+      delete storage[name];
     },
   };
 }
