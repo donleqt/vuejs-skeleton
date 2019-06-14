509d6394306dfac0394958d42fdf76a1215a636a
Don Le Quy <don.lequy@elinext.com>
2019-06-14 18:15:04 +0700

    donlq - updates - INIT FROM SKELETON - refactor default style

--- src/services/api/common.js
+++ src/services/api/common.js
@@ -9,28 +9,7 @@ const makePagesLink = array =>
 
 export class CommonApiService extends RestApiAbstract {
   async getWebsiteConfig() {
-    const resp = await this.apiCaller.get(apiPaths.general.config);
-
-    // parse response
-    const data = {
-      logo: resp.data.logo,
-      mainMenu: makePagesLink(resp.data.main_menu),
-      footerMenu: makePagesLink(resp.data.footer_menu),
-      staticPages: makePagesLink(resp.data.static_pages),
-      privacyPages: makePagesLink(resp.data.privacy_pages),
-      specialPages: makePagesLink(resp.data.special_pages),
-      socialLinks: resp.data.social_links,
-      themeColors: resp.data.colors,
-    };
-
-    if (resp.data.seo) {
-      data.seo = {
-        templateTitle: resp.data.seo.template_title || 'Vue - %s',
-        meta: resp.data.meta,
-      };
-    }
-
-    return data;
+    return Promise.resolve(true);
   }
 }
 
