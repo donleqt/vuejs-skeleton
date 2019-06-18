dd326f6e0ed3e80c3fe922e041c3f650b3f8e684
Don Le Quy <don.lequy@elinext.com>
2019-06-18 10:22:18 +0700

    donlq - updates - shells install

--- .shells/install.sh
+++ .shells/install.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-echo -e "\033[32mCopy yarn.lock...\033[0m"
-cp yarn.lock.example yarn.lock
 echo -e "\033[32mInstall node modules ...\033[0m"
 yarn install
 echo -e "\033[32mClone config files ...\033[0m";
